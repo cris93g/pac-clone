32f6e7d8c0e2468f07bc89dab5803f5dfddf53d7
cris93g <37388115+cris93g@users.noreply.github.com>
2019-09-03 22:55:03 -0500

    late night

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Nav from './components/Nav/Nav';
 import { HashRouter } from 'react-router-dom';
 import routes from './routes';
 import Footer from './components/Footer/Footer';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 class App extends Component {
 	render() {
 		return (
-			<HashRouter>
-				<Nav />
-				{routes}
-				<Footer />
-			</HashRouter>
+			<Provider store={store}>
+				<HashRouter>
+					<Nav />
+					{routes}
+					<Footer />
+				</HashRouter>
+			</Provider>
 		);
 	}
 }
