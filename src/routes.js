ebee0ae8882d7369ddf1e1db336ab8e8f7055cb3
cris93g <37388115+cris93g@users.noreply.github.com>
2019-09-05 23:42:40 -0500

    men-women page showing cards

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './Views/Home/Home';
-import SingleItem from './Views/SingleItem/SingleItem';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Views/Home/Home";
+import SingleItem from "./Views/SingleItem/SingleItem";
+import Men from "./Views/Men/Men";
+import Women from "./Views/Women/Women";
 export default (
-	<Switch>
-		<Route component={Home} exact path="/" />
-		<Route path="/item/:id" component={(props) => <SingleItem timestamp={new Date().toString()} {...props} />} />
-	</Switch>
+  <Switch>
+    <Route component={Home} exact path="/" />
+    <Route component={Men} exact path="/men" />
+    <Route component={Women} exact path="/women" />
+    <Route
+      path="/item/:id"
+      component={props => (
+        <SingleItem timestamp={new Date().toString()} {...props} />
+      )}
+    />
+  </Switch>
 );
